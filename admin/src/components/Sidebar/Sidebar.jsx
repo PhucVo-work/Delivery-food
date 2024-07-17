c99678eb21d02c00955be9f1f7c814c707f4c57a
PhucVo-work <Phuc111239@gmail.com>
2024-07-17 22:52:35 +0700

    Create router admin and create ui for add page

--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -3,24 +3,25 @@ import './Sidebar.css'
 import { assets } from '../../assets/assets'
 import { MdFastfood } from "react-icons/md";
 import { TbShoppingBagEdit, TbShoppingBagPlus } from "react-icons/tb";
+import { NavLink } from 'react-router-dom';
 
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className="sidebar-options">
-        <div className="sidebar-option">
-        <TbShoppingBagPlus className="icon" />
-          <p>Thêm món ăn</p>
-        </div>
-        <div className="sidebar-option">
+        <NavLink to='/add' className="sidebar-option">
+          <TbShoppingBagPlus className="icon" />
+            <p>Thêm món ăn</p>
+        </NavLink>
+        <NavLink to='/list' className="sidebar-option">
           <MdFastfood className="icon" />
-          <p>Danh sách các món ăn</p>
-        </div>
-        <div className="sidebar-option">
+          <p>Danh sách món ăn</p>
+        </NavLink>
+        <NavLink to='/orders' className="sidebar-option">
           <TbShoppingBagEdit className="icon"/>
           <p>Đơn đặt hàng</p>
-        </div>
+        </NavLink>
       </div>
     </div>
   )
--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
-import './Add.css'
+import React from "react";
+import "./Add.css";
+import { assets } from "../../assets/assets";
 
 const Add = () => {
   return (
-    <div>
-      
+    <div className="add">
+      <form className="add">
+        <form className="flex-col">
+          <div className="add-image-upload flex-col">
+            <p>Tải ảnh lên</p>
+            <label htmlFor="image">
+              <img src={assets.upload_area} alt="" />
+            </label>
+            <input type="file" id="image" hidden required />
+          </div>
+          <div className="add-product-name flex-col">
+            <p>Tên món ăn</p>
+            <input type="text" name="tên" placeholder="Nhập ở đây"/>
+          </div>
+          <div className="add-product-description flex-col">
+            <p>Mô tả món ăn</p>
+            <textarea name="Mô tả" rows='6' placeholder="Viết mô tả ở đây" required></textarea>
+          </div>
+          <div className="add-category-price">
+            <div className="add-category flex-col">
+              <p>Loại món ăn</p>
+              <select name="Loại">
+                <option value="Salad">Salad</option>
+                <option value="Cuốn">Cuốn</option>
+                <option value="Tráng miệng">Tráng miệng</option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+                <option value=""></option>
+              </select>
+            </div>
+          </div>
+        </form>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Add
+export default Add;
