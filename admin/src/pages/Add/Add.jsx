b6eebc2bb2224bef3e9bbb0ca19508e9cd950850
PhucVo-work <Phuc111239@gmail.com>
2024-07-18 23:26:47 +0700

    css sidebar

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -4,9 +4,9 @@ import { assets } from "../../assets/assets";
 
 const Add = () => {
   return (
-    <div className="add">
+    <div className="">
       <form className="add">
-        <form className="flex-col">
+        <form className="flex-row">
           <div className="add-image-upload flex-col">
             <p>Tải ảnh lên</p>
             <label htmlFor="image">
@@ -14,28 +14,60 @@ const Add = () => {
             </label>
             <input type="file" id="image" hidden required />
           </div>
-          <div className="add-product-name flex-col">
-            <p>Tên món ăn</p>
-            <input type="text" name="tên" placeholder="Nhập ở đây"/>
-          </div>
-          <div className="add-product-description flex-col">
-            <p>Mô tả món ăn</p>
-            <textarea name="Mô tả" rows='6' placeholder="Viết mô tả ở đây" required></textarea>
-          </div>
-          <div className="add-category-price">
-            <div className="add-category flex-col">
-              <p>Loại món ăn</p>
-              <select name="Loại">
-                <option value="Salad">Salad</option>
-                <option value="Cuốn">Cuốn</option>
-                <option value="Tráng miệng">Tráng miệng</option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-              </select>
+          <div>
+            <div className="add-product-name flex-col">
+              <p>Tên món ăn</p>
+              <input type="text" name="tên" placeholder="Nhập ở đây" />
             </div>
+            <div className="add-product-description flex-col">
+              <p>Mô tả món ăn</p>
+              <textarea
+                name="Mô tả"
+                rows="6"
+                placeholder="Viết mô tả ở đây"
+                required
+              ></textarea>
+            </div>
+            <div className="add-category-price">
+              <div className="add-category flex-col">
+                <p>Loại món ăn</p>
+                <select name="Loại">
+                  <option value="Salad">Salad</option>
+                  <option value="Cuốn">Cuốn</option>
+                  <option value="Tráng miệng">Tráng miệng</option>
+                  <option value="Sandwich">Sandwich</option>
+                  <option value="Mì">Mì</option>
+                  <option value="Pasta">Pasta</option>
+                  <option value="Bánh ngọt">Bánh ngọt</option>
+                  <option value="Thuần chay">Thuần chay</option>
+                </select>
+              </div>
+              <div className="add-category-price">
+                <div className="add-category flex-col">
+                  <p>Khẩu phần</p>
+                  <select name="Loại">
+                    <option value="1-2 người">1 - 2 người</option>
+                    <option value="2-3 người">2- 3 người</option>
+                  </select>
+                </div>
+              </div>
+              <div className="add-category-price">
+                <div className="add-category flex-col">
+                  <p>Thời gian chế biến</p>
+                  <select name="Loại">
+                    <option value="5-10 phút">5-10 phút</option>
+                    <option value="20-30 phút">20-30 phút</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="add-price flex-col">
+              <p>Giá món ăn</p>
+              <input type="Number" name="price" placeholder="VNĐ" />
+            </div>
+            <button type="submit" className="add-btn">
+              Thêm món ăn
+            </button>
           </div>
         </form>
       </form>
