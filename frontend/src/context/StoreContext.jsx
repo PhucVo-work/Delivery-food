9540b9afff884c723ada8b40e8a56d4a9cdb0531
PhucVo-work <Phuc111239@gmail.com>
2024-08-14 17:40:52 +0700

    problem deploy

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const [menu, setMenu] = useState("home");
-  const url = "http://localhost:4000";
+  const url = "https://delivery-food-backend-kxgz.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
